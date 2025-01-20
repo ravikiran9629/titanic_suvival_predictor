d9098c46227b6d02cc24dccaca7606930b79c8be
ravikiran9629 <118425431+ravikiran9629@users.noreply.github.com>
2025-01-21 03:27:32 +0530

    Update Titanic_survival_predictor.py

--- Titanic_survival_predictor.py
+++ Titanic_survival_predictor.py
@@ -14,28 +14,28 @@ with open('logistic_regression_model.pkl', 'rb') as f:
 
 # Streamlit app
 st.title('Titanic Survival Predictor')
-def user_input_features():
-    age = st.number_input('Age')
-    fare = st.number_input('Fare')
-    sex = st.selectbox('Sex', ['male', 'female'])
-    embarked = st.selectbox('Embarked', ['Cherbourg', 'Queenstown', 'Southampton'])
-    pclass = st.selectbox('Passenger Class', [1, 2, 3])
-    sibsp = st.slider('Siblings/Spouse', 0, 10, 0)
-    parch = st.slider('Parents/Childern', 0, 10, 0)
+
+age = st.number_input('Age')
+fare = st.number_input('Fare')
+sex = st.selectbox('Sex', ['male', 'female'])
+embarked = st.selectbox('Embarked', ['Cherbourg', 'Queenstown', 'Southampton'])
+pclass = st.selectbox('Passenger Class', [1, 2, 3])
+sibsp = st.slider('Siblings/Spouse', 0, 10, 0)
+parch = st.slider('Parents/Childern', 0, 10, 0)
     
-    data = {
+data = {
     'Pclass': [pclass],
     'SibSp': [sibsp],
     'Parch': [parch],
     'Female': [1 if sex == 'female' else 0],
-    'Cherbourg': [1 if embarked == 'C' else 0],
-    'Queenstown': [1 if embarked == 'Q' else 0],
-    'Southampton': [1 if embarked == 'S' else 0],
+    'C': [1 if embarked == 'Cherbourg' else 0],
+    'Q': [1 if embarked == 'Queenstown' else 0],
+    'S': [1 if embarked == 'SouthamptonS' else 0],
     'Age': [age],
     'Fare': [fare]
-    }
-    input_data = pd.DataFrame(data,index=[0])
-    return input_data
+}
+input_data = pd.DataFrame(data,index=[0])
+
 
 
 # Standardize the input data
@@ -43,4 +43,11 @@ input_data[['Age','Fare']] = scaler.transform(input_data[['Age','Fare']] )
 
 # Predict
 survival_proba = loaded_model.predict_proba(input_data)
-st.write('Yes' if survival_proba[0][1] > 0.5 else 'No')
+
+
+if st.button("Show Result"):
+    # Code to execute after button click
+    if survival_proba[0][1] > 0.5:
+        st.write("Survived")
+    else:
+        st.write("Not Survived")
